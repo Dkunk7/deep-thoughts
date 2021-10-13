5687546cced96bcaa7dec23db464da400aaa07ba
Dylan K <drkunkel7@gmail.com>
2021-10-13 11:14:06 -0500

    route work!

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ThoughtList = ({ thoughts, title }) => { // props destructured into thoughts array and title to avoid using props.thoughts and props.title
     if (!thoughts.length) {
@@ -12,15 +13,23 @@ const ThoughtList = ({ thoughts, title }) => { // props destructured into though
                 thoughts.map(thought => (
                     <div key = {thought._id} className = "card mb-3"> {/* Use key to help React know which data to re-render if something changes */}
                         <p className = "card-header">
-                            {thought.username}
+                            <Link
+                                to = {`/profile/${thought.username}`}
+                                style = {{ fontWeight: 700 }}
+                                className = "text-light"
+                            >
+                                {thought.username}
+                            </Link>{' '}
                             thought on {thought.createdAt}
                         </p>
                         <div className = "card-body">
-                            <p>{thought.thoughtText}</p>
-                            <p className = "mb-0">
-                                Reactions: {thought.reactionCount} || Click to{' '}
-                                {thought.reactionCount ? 'see' : 'start' } the discussion! {/* if there are reactions, it says click to SEE the discussion; if not, it says click to START */}
-                            </p>
+                            <Link to = {`/thought/${thought._id}`}>
+                                <p>{thought.thoughtText}</p>
+                                <p className = "mb-0">
+                                    Reactions: {thought.reactionCount} || Click to{' '}
+                                    {thought.reactionCount ? 'see' : 'start' } the discussion! {/* if there are reactions, it says click to SEE the discussion; if not, it says click to START */}
+                                </p>
+                            </Link>
                         </div>
                     </div>
                 ))}
